a274b6ac864e4c7cf7af3305af2453f7cbdd630e
2106055MuhammadGofur <127308482+2106055MuhammadGofur@users.noreply.github.com>
2023-03-24 13:42:58 +0700

    Add files via upload

---
+++ 2106055_MuhammadGofur_SilsilahKeluarga.pl
@@ -0,0 +1,24 @@
+ortu(david, liza).
+ortu(amy, liza).
+ortu(david, john).
+ortu(amy, john).
+ortu(jack, susan).
+ortu(karen, susan).
+ortu(john, peter).
+ortu(susan, peter).
+ortu(john, mary).
+ortu(susan, mary).
+ortu(jack, ray).
+ortu(karen, ray).
+
+laki(david).
+laki(john).
+laki(peter).
+laki(jack).
+laki(ray).
+
+prpuan(amy).
+prpuan(liza).
+prpuan(susan).
+prpuan(mary).
+prpuan(karen).
